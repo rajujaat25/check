5bed716af6cbe067d6b0c9ace79253abf79dd5b8
rajujaat25 <72685396+rajujaat25@users.noreply.github.com>
2023-12-10 15:55:24 +0530

    Update Resource.groovy

--- vars/Resource.groovy
+++ vars/Resource.groovy
@@ -1,7 +1,7 @@
 def call()
 {
   def content = libraryResource "bash.sh"
-  WriteFile file: "pro.sh", text: content
+  writeFile file: "pro.sh", text: content
   sh "ls -a ./pro.sh"
   sh "bash ./pro.sh"
 }
